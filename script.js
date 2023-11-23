ae7af2f32159bce8f8339cea6e902b3c1c0c5965
xdnkpp <144899276+xdnkpp@users.noreply.github.com>
2023-11-23 16:01:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function imgSlider(anything){
+    document.querySelector('.minion').src = anything;
+}
+
+videojs(document.querySelector(video-js))
+
+$(document).ready(function() { 
+    $("#menu-button").click(function() { 
+        $("#menu").animate({ 
+            width: 'toggle' 
+        }); 
+    }); 
+});
+
+
